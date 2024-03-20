cba94ebbe08ca5e15ee912ffd825f8d1d084ba73
santiagofep <santiagofe.p@gmail.com>
2024-03-19 21:21:01 -0500

    Add VSCode and Babel configuration files, update webpack config and package.json

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
