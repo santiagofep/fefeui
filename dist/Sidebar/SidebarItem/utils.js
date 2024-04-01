581c372e57b99031fb768b810487714a2ac4e4f5
santiagofep <santiagofe.p@gmail.com>
2024-04-01 14:57:04 -0500

    Remove console.log statement and update package version

--- dist/Sidebar/SidebarItem/utils.js
+++ dist/Sidebar/SidebarItem/utils.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getButtonVariant = void 0;
 const getButtonVariant = (isActive, childrenIsActive) => {
-    console.log(isActive);
     if (isActive)
         return "solid";
     if (childrenIsActive)
